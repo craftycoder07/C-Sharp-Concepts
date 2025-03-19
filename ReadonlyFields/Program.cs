7e764aa8e7e89aad9188023e1a8b8112cf6231e1
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2025-03-19 17:02:57 -0400

    Added Readonly fields chapter

---
+++ ReadonlyFields/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+public class Foo
+{
+    /*
+     * 1) Readonly fields are initialized in constructor
+     * 2) Once initialized, they cannot be assigned value anywhere else in the code
+     * 3) Best use case for it is loading configuration values.
+     *    They are not know when developing SW, and you dont want to change them during execution.
+     */
+
+    public readonly int _testNumber = 20;
+    
+    public Foo()
+    {
+        _testNumber = 10;
+    }
+}

ff6088ba63b4401f7b02c59a8090a2c797ebc26d
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2025-03-20 19:18:41 -0400

    Added chapter on ANonymous types

---
+++ AnonymousTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ * ANONYMOUS TYPES
+ * 1) Anonymous types are types without class name. You just use 'var' and 'new' keywords to create objects of it.
+ * 2) If two anonymous types has same properties then they are of same type.
+ *    You can do anonymous1 = anonymous2 (basically assignment)
+ */
+var engineer = new
+{
+    FirstName = "John",
+    LastName = "Doe"
+};
+
+var doctor = new
+{
+    FirstName = "Sam",
+    LastName = "Jackson"
+};
+
+/*
+ * If you use other objects to assign values to properties of anonymous object, you don't have to declare property names.
+ * They are automatically inferred.
+ */
+var dentist = new
+{
+    doctor.FirstName,
+    doctor.LastName
+};
+
+/*
+ * If you try to check type of anonymous type, you will see that compiler generates random class name.
+ * Thus don't use type reflection on anonymous type.
+ */
+Type type = dentist.GetType();
+Console.WriteLine($"Type Name: {type.Name}");
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. [Constructor With Base Keyword](./ConstructorWithBaseKeyword/)
 3. [Readonly Fields](./ReadonlyFields/)
 4. [Properties](./Properties/)
+5. [Anonymous Types](./AnonymousTypes/)

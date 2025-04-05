7271538a404e4286ed2b9c7d14c92484f8b63bd2
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2025-04-04 22:54:28 -0400

    Modified chapter on Access Modifiers

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -1,26 +1,39 @@
 ﻿using AccessModifierLib;
 
 //Can't access private members anywhere
-//Shape.x = 1;
+//Console.WriteLine(Shape.x);
 
 //Can access public member
-Shape.y = 2;
+Console.WriteLine(Shape.y);
 
-//Can't access protected member. Only accessible from derived class
-//Shape.z = 3;
-Rectangle rect = new Rectangle();
-rect.Draw();
+//Can't access internal members
+//Console.WriteLine(Shape.z);
 
-//Since Square class is 'internal' in 'AccessModifierLib' library, it is not accessible in 'AccessModifier'
-//Developer will get compilation error.
-//Square s = new Square();
+public class Circle : Shape
+{
+    public static void Draw2()
+    {
+        //protected member is accessible in derived class in different assembly
+        Console.WriteLine(a);
+        
+        //'protected internal' is accessible in derived class of different assembly
+        Console.WriteLine(b);
+        
+        //'private protected' NOT accessible in derived class of different assembly
+        //Console.WriteLine(c);
+    }
+}
 
-//'X' is not accessible here as it is declared protected.
-//Shape1 s = new Shape1();
-//s.X;
-
-// Circle c = new Circle();
-// c.Y;
-
+public class Cone
+{
+    public static void Draw2()
+    {
+        //'protected internal' is NOT accessible in other class of different assembly
+        //Console.WriteLine(b);
+        
+        //'private protected' NOT accessible in other class of different assembly
+        //Console.WriteLine(c);
+    }
+}
 
 

b5fad901c71dc1cf9c27181fcc0b627619d5327d
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2025-04-29 19:13:41 -0400

    Updated Garbage collection to demonstrate how unmanged ojects are NOT cleared by GC

--- GarbageCollectorDemo/MyClass.cs
+++ GarbageCollectorDemo/MyClass.cs
@@ -1,6 +1,73 @@
-﻿namespace GarbageCollectorDemo
+﻿using System.Runtime.InteropServices;
+
+namespace GarbageCollectorDemo
 {
-    internal class MyClass
+    internal class ManagedObjectCleanedByGC
     {
+    }
+
+    internal class UnmanagedObjectNotCleanedByGC
+    {
+        List<IntPtr> allocatedBlocks = new List<IntPtr>();
+        
+        public UnmanagedObjectNotCleanedByGC(long i)
+        {
+            const int numBlocks = 1000;
+            const int blockSizeInMB = 10;
+            const int blockSizeInBytes = blockSizeInMB * 1024 * 1024;
+            
+            IntPtr ptr = Marshal.AllocHGlobal(blockSizeInBytes);
+            // Optional: Write something to the memory to ensure it's committed
+            unsafe
+            {
+                byte* bytePtr = (byte*)ptr.ToPointer();
+                for (int j = 0; j < blockSizeInBytes; j += 4096) // write once per memory page
+                    bytePtr[j] = 1;
+            }
+            allocatedBlocks.Add(ptr);
+
+            Console.WriteLine($"Allocated {((i + 1) * blockSizeInMB)} MB total");
+            System.Threading.Thread.Sleep(100); // Slow down to observe in Task Manager
+        }
+    }
+    
+    internal class UnmanagedObjectWithFinalizer
+    {
+        List<IntPtr> allocatedBlocks = new List<IntPtr>();
+        
+        public UnmanagedObjectWithFinalizer(long i)
+        {
+            const int numBlocks = 1000;
+            const int blockSizeInMB = 10;
+            const int blockSizeInBytes = blockSizeInMB * 1024 * 1024;
+            
+            IntPtr ptr = Marshal.AllocHGlobal(blockSizeInBytes);
+            // Optional: Write something to the memory to ensure it's committed
+            unsafe
+            {
+                byte* bytePtr = (byte*)ptr.ToPointer();
+                for (int j = 0; j < blockSizeInBytes; j += 4096) // write once per memory page
+                    bytePtr[j] = 1;
+            }
+            allocatedBlocks.Add(ptr);
+
+            Console.WriteLine($"Allocated {((i + 1) * blockSizeInMB)} MB total");
+            System.Threading.Thread.Sleep(100); // Slow down to observe in Task Manager
+        }
+
+        ~UnmanagedObjectWithFinalizer()
+        {
+            Console.WriteLine("Finalizer called. Releasing unmanaged memory...");
+
+            foreach (var ptr in allocatedBlocks)
+            {
+                if (ptr != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(ptr);
+                }
+            }
+
+            allocatedBlocks.Clear();
+        }
     }
 }
--- GarbageCollectorDemo/Program.cs
+++ GarbageCollectorDemo/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Please press 1 for Demo of GC for managed objects");
             Console.WriteLine("Please press 2 for Demo of GC for primitive types");
+            Console.WriteLine("Please press 3 for Demo of GC for unmanaged types");
+            Console.WriteLine("Please press 4 for Demo of GC for unmanaged types with finalizers");
             string? selectedOption = Console.ReadLine();
 
             switch (selectedOption)
@@ -18,6 +20,14 @@
                     //Demo GC for primitive types
                     DemoGCForPrmitiveTypes();
                     break;
+                case "3":
+                    //Demo GC for unmanaged types
+                    DemoGCForUnmangedObjects();
+                    break;
+                case "4":
+                    //Demo GC for unmanaged types with finalizers
+                    DemoGCForUnmangedObjectsWithFinalizers();
+                    break;
                 default:
                     Console.WriteLine("Incorrect selection. Please run the app again.");
                     break;
@@ -25,7 +35,7 @@
         }
 
         /// <summary>
-        /// This method demostrates how unreferenced managed objects are cleaned by GC.
+        /// This method demonstrates how unreferenced managed objects are cleaned by GC.
         /// </summary>
         static void DemoGCForManagedObjects()
         {
@@ -38,16 +48,19 @@
                 /*
                  * This is managed object.
                  * As soon as for loop goes to next iteration object becomes unreferenced. (Becomes eligible for GC)
-                 * GC won't immediatly kick in.
+                 * GC won't immediately kick in.
                  * When there is strain on memory GC will collect unreferenced objects.
                  * In performance counter, you will see GC heap size going UP and DOWN indicating that unreferenced managed objects are cleaned by GC
                  */
-                MyClass myClass = new MyClass();
+                ManagedObjectCleanedByGC myClass = new ManagedObjectCleanedByGC();
             }
             Console.WriteLine("After FOR loop");
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// This method demonstrates how primitives are allocated on stack and not cleaned by GC.
+        /// </summary>
         static void DemoGCForPrmitiveTypes()
         {
             Console.WriteLine("Press enter to start GC for primitive type demo");
@@ -59,6 +72,52 @@
                  * You will very small change in size of heap memeory unlike 'DemoGCForManagedObjects' where heap memory size changes drastically.
                  */
                 int j = 0;
+            }
+            Console.WriteLine("After FOR loop");
+            Console.ReadKey();
+        }
+        
+        /// <summary>
+        /// This method demonstrates how unmanaged objects not cleaned by GC even though they are allocated on heap.
+        /// </summary>
+        static void DemoGCForUnmangedObjects()
+        {
+            Console.WriteLine("Press enter to start GC for un-managed object demo");
+            Console.ReadKey();
+            for (long i = 0; i < 2000000; i++)
+            {
+                Console.WriteLine(i);
+
+                /*
+                 * This is un-managed object.
+                 * As soon as for loop goes to next iteration object becomes unreferenced. (Becomes eligible for GC)
+                 * When GC kick in, it will NOT collect unmanaged objects, leaving behind memory leak.
+                 * In performance counter, you will see GC heap size won't change indicating that unmanaged objects are NOT cleaned by GC
+                 */
+                UnmanagedObjectNotCleanedByGC myClass = new UnmanagedObjectNotCleanedByGC(i);
+            }
+            Console.WriteLine("After FOR loop");
+            Console.ReadKey();
+        }
+        
+        /// <summary>
+        /// This method demonstrates how unmanaged objects not cleaned by GC even though they are allocated on heap.
+        /// </summary>
+        static void DemoGCForUnmangedObjectsWithFinalizers()
+        {
+            Console.WriteLine("Press enter to start GC for un-managed object demo with finalizers");
+            Console.ReadKey();
+            for (long i = 0; i < 2000000; i++)
+            {
+                Console.WriteLine(i);
+
+                /*
+                 * This is un-managed object.
+                 * As soon as for loop goes to next iteration object becomes unreferenced. (Becomes eligible for GC)
+                 * When GC kick in, it will NOT collect unmanaged objects, leaving behind memory leak.
+                 * In performance counter, you will see GC heap size won't change indicating that unmanaged objects are NOT cleaned by GC
+                 */
+                UnmanagedObjectWithFinalizer myClass = new UnmanagedObjectWithFinalizer(i);
             }
             Console.WriteLine("After FOR loop");
             Console.ReadKey();
